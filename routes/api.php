f2d4e789218140fb38ea648437ffa0cb4d245f81
Ruan Carlos Passeto <ruanpasseto@gmail.com>
2025-09-23 14:34:45 -0300

    create auth controller and api.php

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\AppointmentController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('patients', PatientController::class);
+
+    Route::apiResource('appointments', AppointmentController::class);
+});
+
